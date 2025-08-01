69a932bce0568301a1fdb5abf6610134e2dbbb40
jiaql <jiaql@founcer.com>
2025-08-01 14:25:39 +0800

    fix: update database host and port in aione-values.yaml

---
+++ script/port-forward.sh
@@ -0,0 +1,2 @@
+
+kubectl port-forward --address 0.0.0.0 ap24d29gfbbdhjnsp5wc-n0-0 8080:8080 -n flytesnacks-development
---
+++ script/pull.sh
@@ -0,0 +1,1 @@
+crictl pull ghcr.fzyun.io/flyteorg/flytekit:flyteinteractive-latest
