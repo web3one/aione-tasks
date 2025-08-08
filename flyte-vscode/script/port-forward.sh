64a03755ecc0e3060332c3488fdaac6201b3e92a
jiaql <jiaql@founcer.com>
2025-08-08 09:58:04 +0800

    Remove unused build argument `VERSION` from Docker build script

---
+++ flyte-vscode/script/port-forward.sh
@@ -0,0 +1,2 @@
+
+kubectl port-forward --address 0.0.0.0 a2mfbnpczn8gqxrk24l6-n0-0 8080:8080 -n flytesnacks-development
--- script/port-forward.sh
+++
@@ -1,2 +0,0 @@
-
-kubectl port-forward --address 0.0.0.0 ap24d29gfbbdhjnsp5wc-n0-0 8080:8080 -n flytesnacks-development
